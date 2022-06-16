773c1962fe40bd19c81f5b2ec862d7f780c156d3
Matheus <matheusdomingues10@gmail.com>
2022-06-16 18:55:20 -0300

    Prompt recebendo quantidade de pares feito

---
+++ script.js
@@ -0,0 +1,29 @@
+let numeroPar = null;
+
+function ehPar(){
+    while (numeroPar > 14 || numeroPar < 4 || numeroPar % 2 !== 0) 
+    {numeroPar = prompt("Quantas cartas? (Apenas pares de 4 a 14)");}
+}
+
+ehPar();
+
+
+// (Escolha um número par entre 4 a 14 cartas)
+
+// se quantidade de cartas for multiplo de 2 = aceita
+// senao repita pergunta
+
+// com a quantidade informada, faça aparecer a quantidade de duplas de cartas solicitadas viradas PARA BAIXO
+// embaralhe as cartas usando .sort e Math.random
+
+// carta deve ser virada ao clique
+// sendo a primeira do par, deve permanecer virada
+
+// ao virar a segunda carta:
+// se for ambas iguais, ambas permanecem viradas
+// senão, ambas desviram automaticamente APÓS 1 SEGUNDO
+
+// alert de fim de jogo quando todas as cartas foram viradas
+// contendo um texto com o NÚMERO DE TENTATIVAS
+
+// BONUS: após o alert do final, prompt de reiniciar
