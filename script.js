b53ef55ae51dff4a340c6eeaf88d3e614460007e
Matheus <matheusdomingues10@gmail.com>
2022-06-19 14:50:30 -0300

    Redução de Linhas na Criação de Cartas, organização de Funções e Adição de Comparação de Pares

--- script.js
+++ script.js
@@ -1,69 +1,73 @@
+
 let numeroPar = null;
+let elemento = document.querySelector(".conteiner-carta");
+let primeiraCarta = null;
+let segundaCarta = null;
 
 const gifs = ['bobrossparrot','explodyparrot','fiestaparrot','metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
 
-let elemento = document.querySelector(".conteiner-carta");
-
-function ehPar(){
+function baralhoPar(){
     while (numeroPar >14  || numeroPar < 4 || numeroPar % 2 !== 0) 
     {numeroPar = prompt("Quantas cartas? (Apenas pares de 4 a 14)");}
     iniciaJogo();
 }
 
-ehPar();
+baralhoPar();
 
 function iniciaJogo(){
+  
   let baralho = [];
-  for (let i = 0; i < numeroPar; i+=2) {
-    baralho.push(`<div class="carta" onclick="viraCartas(this)">
-          <div class="carta-frente">
-            <img src="img/front.png"/>
-          </div>
-          <div class="carta-verso">
-            <img src="gifs/${gifs[i / 2]}.gif"/>
-          </div>
-        </div>`)
-    baralho.push(
-      `<div class="carta" onclick="viraCartas(this)">
-          <div class="carta-frente">
-            <img src="img/front.png"/>
-          </div>
-          <div class="carta-verso">
-            <img src="gifs/${gifs[i / 2]}.gif"/>
-          </div>
-        </div>`)
+  for (let i = 0; i < numeroPar/2; i++) {
+    const carta = criarCarta(i);
+    baralho.push(carta);
+    baralho.push(carta);    
   }
+
+  function criarCarta(iCarta){
+    const parrot = gifs[iCarta];
+    return `<div class="carta" onclick="viraCarta(this)">
+              <div class="carta-frente">
+                <img src="img/front.png"/>
+              </div>
+              <div class="carta-verso">
+                <img src="gifs/${parrot}.gif"/>
+              </div>
+            </div>`
+  }
+  
   baralho.sort(comparador);
-  for (let j = 0; j < numeroPar; j++) {
-    elemento.innerHTML += baralho[j];      
+  for (let j = 0; j < numeroPar; j++) 
+  {elemento.innerHTML += baralho[j];}
+
+  function comparador() {
+  return Math.random() - 0.5;}
   }
-}
-
-function comparador() {
-    return Math.random() - 0.5;
+ 
+  function viraCarta(carta) {
+  carta.classList.add("virada");
+  if(primeiraCarta === null){
+    primeiraCarta = carta;
+  } else {
+    segundaCarta = carta;
+    comparaCarta();
   }
 
-function viraCartas(cartaVirada) {
-  cartaVirada.classList.add("virada");
+  function comparaCarta(){
+    if(primeiraCarta.innerHTML === segundaCarta.innerHTML){
+      primeiraCarta.classList.add("combina");
+      segundaCarta.classList.add("combina");
+      primeiraCarta = null;
+      segundaCarta = null;
+    } else {
+      setTimeout(desviraCarta, 1000);
+    }
+  }
+
+  function desviraCarta(){
+    primeiraCarta.classList.remove("virada");
+    segundaCarta.classList.remove("virada");
+    primeiraCarta = null;
+    segundaCarta = null;
+  }
+
 }
-
-
-// (Escolha um número par entre 4 a 14 cartas)
-
-// se quantidade de cartas for multiplo de 2 = aceita
-// senao repita pergunta
-
-// com a quantidade informada, faça aparecer a quantidade de duplas de cartas solicitadas viradas PARA BAIXO
-// embaralhe as cartas usando .sort e Math.random
-
-// carta deve ser virada ao clique
-// sendo a primeira do par, deve permanecer virada
-
-// ao virar a segunda carta:
-// se for ambas iguais, ambas permanecem viradas
-// senão, ambas desviram automaticamente APÓS 1 SEGUNDO
-
-// alert de fim de jogo quando todas as cartas foram viradas
-// contendo um texto com o NÚMERO DE TENTATIVAS
-
-// BONUS: após o alert do final, prompt de reiniciar
